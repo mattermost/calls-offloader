d45eb394b1fe74feb3b0573cf3945ad865766b83
Claudio Costa <cstcld91@gmail.com>
2022-11-28 13:46:48 -0600

    Improvements (#2)

--- service/client.go
+++ service/client.go
@@ -96,7 +96,7 @@ func NewClient(cfg ClientConfig, opts ...ClientOption) (*Client, error) {
 		MaxConnsPerHost:       100,
 		MaxIdleConns:          100,
 		MaxIdleConnsPerHost:   100,
-		ResponseHeaderTimeout: 10 * time.Second,
+		ResponseHeaderTimeout: 2 * time.Minute,
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   1 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
